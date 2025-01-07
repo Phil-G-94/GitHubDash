d36c796f1943f3c629e9cfc67318b4af7717753f
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-01-07 13:05:17 +0000

    add overview stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { getRepos } from "./data/data";
 import RepoCard from "./components/UI/RepoCard";
 
+// await repo data
 const repos = await getRepos();
 
-const sortedRepos = repos.sort(
-    (a, b) => new Date(b.updated_at) - new Date(a.updated_at)
-);
+// sort in descending order
+repos.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
 
+const totalRepos = repos.length;
+
+// slice top six - based on most recently updated
+const topSixRepos = repos.slice(0, 6);
+
+// slice last updated - out of top six
+const recentlyUpdatedRepo = topSixRepos.slice(0, 1)[0];
+
+const recentlyUpdatedRepoDate = new Date(
+    recentlyUpdatedRepo.updated_at
+).toLocaleDateString();
+
+// repo size in KB
 const repoSizeInKB = repos.reduce((sum, repo) => sum + repo.size, 0);
 
+// convert to MB and fix to two decimal places
 const repoSizeInMB = (repoSizeInKB / 1024).toFixed(2) + " " + "MB";
 
 function App() {
@@ -16,19 +30,63 @@ function App() {
         <>
             <h1 className="text-red-500 text-center">GitHubDash</h1>
 
+            {/* */}
             <h2 className="text-red-500 text-center">
                 Repositories Overview
             </h2>
+            <section className="flex justify-center gap-2">
+                <div>
+                    <h3>Total Repositories:</h3>
+                    <p>{`${totalRepos}`}</p>
+                </div>
+                <div>
+                    <h3>Total Size:</h3>
+                    <p>{`${repoSizeInMB}`}</p>
+                </div>
+                <div>
+                    <h3>Recently Updated:</h3>
+                    <p>{`${recentlyUpdatedRepo.name}`}</p>
+                </div>
+                <div>
+                    <h3>Updated At:</h3>
+                    <p>{`${recentlyUpdatedRepoDate}`}</p>
+                </div>
+            </section>
+            {/* */}
 
-            <div className="flex justify-center">
-                <p>Total Size {`${repoSizeInMB}`}</p>
-            </div>
+            {/* */}
+            <h2 className="text-red-500 text-center">
+                Languages Used
+            </h2>
+            <section>
+                {/* PIE CHART GOES HERE */}
+                <p className="text-center">
+                    This section should contain a pie chart of all the
+                    languages used in my repos. To be built...
+                </p>
+            </section>
 
+            {/* */}
+
+            {/* */}
+            <h2 className="text-red-500 text-center">
+                Commit Timeline (Year)
+            </h2>
+            <section>
+                {/* GRAPH CHART GOES HERE */}{" "}
+                <p className="text-center">
+                    This section should contain a graph chart of
+                    yearly or monthly commits
+                </p>
+            </section>
+            {/* */}
+
+            {/* */}
             <h2 className="text-red-500 text-center"> Top Repos </h2>
 
-            <div className="flex justify-center">
-                <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-3">
-                    {sortedRepos.map((repo) => {
+            <section className="flex justify-center">
+                <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-3 md:grid-cols-3 md:gap-4">
+                    {topSixRepos.map((repo) => {
                         return (
                             <RepoCard
                                 key={repo.node_id}
@@ -37,7 +95,8 @@ function App() {
                         );
                     })}
                 </div>
-            </div>
+            </section>
+            {/* */}
         </>
     );
 }
